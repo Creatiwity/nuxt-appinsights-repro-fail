76776e91f89e06c3c5e699cf39d3a1500ed6e74e
Julien Blatecky <julien.blatecky@consulting-for.edenred.com>
2024-06-04 17:57:51 +0200

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  typescript: {
+    typeCheck: true,
+    strict: true,
+  },
+  modules: ['nuxt-applicationinsights'],
+  runtimeConfig: {
+    test: 'hello world',
+  },
+})
